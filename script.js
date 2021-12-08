b44b1c2c22d0c9f577eadcecbc6875064fe43ee7
SeanvonB <42122062+SeanvonB@users.noreply.github.com>
2021-12-07 21:55:45 -0800

    Add dynamic resizing

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const canvas = document.querySelector("#canvas");
+const base = document.querySelector("#base");
 
 function drawBasic() {
 	this.style.background = "#555";
 }
 
+// Change vw to vh for landscape devices
+function checkRotation() {
+	if (window.innerHeight < window.innerWidth) {
+		canvas.style.width = "65vh";
+		canvas.style.height = "65vh";
+		base.style.width = "65vh";
+	} else {
+		canvas.style.width = "65vw";
+		canvas.style.height = "65vw";
+		base.style.width = "65vw";
+	}
+}
+
 // Populate canvas with tiles
 function createCanvas(size = 16, style = drawBasic) {
+	// Confirm screen orientation
+	checkRotation();
+
 	for (let i = 0; i < size * size; i++) {
 		let div = document.createElement("div");
 		div.classList.add("cell");
@@ -20,6 +37,9 @@ function createCanvas(size = 16, style = drawBasic) {
 		canvas.appendChild(div);
 	}
 }
+
+// Listen for orientation changes and update vh/wh
+window.addEventListener("resize", checkRotation);
 
 // Create initial canvas
 createCanvas();
